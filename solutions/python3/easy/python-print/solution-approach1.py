3078c0ef0b2ab4ba6048102cfbe926cc70177ad9
SAURAV-CODERWORLD <sauravbaluni7@gmail.com>
2026-07-28 11:39:45 +0530

    ✅ Print Function [python3] — approach1

---
+++ solutions/python3/easy/python-print/solution-approach1.py
@@ -0,0 +1,15 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/python-print/problem?isFullScreen=true
+# Problem     Print Function
+# Difficulty  Easy
+# Subdomain   Introduction
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-07-28, 11:39 a.m.
+# ──────────────────────────────────────────────────
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range (n):
+                print(i+1,end="")
