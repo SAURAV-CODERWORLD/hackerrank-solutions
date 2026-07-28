e4979ab91fb938891480f6c4e6473810dd7cd08d
SAURAV-CODERWORLD <sauravbaluni7@gmail.com>
2026-07-28 11:39:08 +0530

    ✅ Loops [python3] — approach1

---
+++ solutions/python3/easy/python-loops/solution-approach1.py
@@ -0,0 +1,15 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true
+# Problem     Loops
+# Difficulty  Easy
+# Subdomain   Introduction
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-07-28, 11:39 a.m.
+# ──────────────────────────────────────────────────
+
+n = int(input())
+
+for i in range(n):
+    print(i * i)
